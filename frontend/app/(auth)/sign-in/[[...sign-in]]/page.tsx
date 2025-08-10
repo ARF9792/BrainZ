f3d6dab0ac06e365a08dc43db0ad50d47d2ea83d
ARF9792 <arf19792@gmail.com>
2025-08-10 17:07:13 +0530

    fix for auth

--- frontend/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ frontend/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,7 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function SignInPage() {
     return (
-        <div className="bg-black min-h-screen flex items-center justify-center">
+        <div className="bg-blue-100 min-h-screen flex items-center justify-center">
             <SignIn />
         </div>
     );
--- frontend/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ frontend/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function SignUpPage() {
     return (
-        <div className="bg-black min-h-screen flex items-center justify-center">
+        <div className="bg-blue-100 min-h-screen flex items-center justify-center">
             <SignUp />
         </div>
     );
